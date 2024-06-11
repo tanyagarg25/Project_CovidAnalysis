afbe5b84d7aa3a67f5135975cbc088c4a24e9f31
tanyagarg25 <tanyagarg25@gmail.com>
2024-06-11 15:36:14 -0700

    Update COVID 19 -Data Exploration.sql

--- COVID 19 -Data Exploration.sql
+++ COVID 19 -Data Exploration.sql
@@ -6,12 +6,12 @@ Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, 
 */
 
 Select *
-From PortfolioProject..CovidDeaths
+From PortfolioProject.CovidDeaths
 Where continent is not null 
 order by 3,4
 
 
--- Select Data that we are going to be starting with
+-- Selecting Data that we are going to be starting with
 
 Select Location, date, total_cases, new_cases, total_deaths, population
 From PortfolioProject.CovidDeaths
